db9db111b515e1e5a8a3898af4d46db74044c8f9
Ripudaman <ripudamansinghthind@gmail.com>
2019-08-15 22:01:13 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.png';
+import logo2 from './logo2.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1>Hello</h1>
-      </header>
+    <div className="container center">
+        <nav className="menu">
+            <h1 className="menu__logo" 
+            style={{
+              backgroundImage: 'url(' + logo + ')'
+            }}>Time Logo</h1>
+
+            <div className="menu__right">
+                <ul className="menu__list">
+                    <li className="menu__list-item"><a class="menu__link menu__link--active" href="#">Home</a></li>
+                    <li className="menu__list-item"><a class="menu__link" href="#">About</a></li>
+                    <li className="menu__list-item"><a class="menu__link" href="#">Portfolio</a></li>
+                    <li className="menu__list-item"><a class="menu__link" href="#">Contact</a></li>
+                </ul>
+                <form className="menu__search-form hide" method="POST">
+                    <input className="menu__search-input" placeholder="Type and hit enter" />
+                </form>
+            </div>
+        </nav>
     </div>
   );
 }
