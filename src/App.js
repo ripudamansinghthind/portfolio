55b73d4ced9a8a33a9db3ab69b4f906abef54adc
Ripudaman <ripudamansinghthind@gmail.com>
2019-08-15 23:29:48 -0700

    redisgn draft

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import logo from './logo.png';
-import logo2 from './logo2.png'
 
 function App() {
   return (
     <div className="container center">
-        <nav className="menu">
-            <h1 className="menu__logo" 
-            style={{
-              backgroundImage: 'url(' + logo + ')'
-            }}>Time Logo</h1>
-
-            <div className="menu__right">
-                <ul className="menu__list">
-                    <li className="menu__list-item"><a class="menu__link menu__link--active" href="#">Home</a></li>
-                    <li className="menu__list-item"><a class="menu__link" href="#">About</a></li>
-                    <li className="menu__list-item"><a class="menu__link" href="#">Portfolio</a></li>
-                    <li className="menu__list-item"><a class="menu__link" href="#">Contact</a></li>
-                </ul>
-                <form className="menu__search-form hide" method="POST">
-                    <input className="menu__search-input" placeholder="Type and hit enter" />
-                </form>
+    <div id="intro">
+    <h3>Hi, I am Felix and I love time</h3>    
+    </div>
+    <div id="circle">
+            <div className="circle__topleft">
+            <h4>ABOUT ME</h4>
             </div>
-        </nav>
+            <div className="circle__topright">
+            <h4>PROJECTS</h4>
+            </div>
+            <div className="circle__botleft">
+            <h4>SOMETHING</h4>
+            </div>
+            <div className="circle__botright">
+              <h4>CONTACT ME</h4>
+            </div>
+            </div>
     </div>
   );
 }
