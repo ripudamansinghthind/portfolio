96b0d0695bc88c94e6271aec39cb0cd8b293681b
Ripudaman <ripudamansinghthind@gmail.com>
2020-04-18 21:27:21 -0700

    small footer changes

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -10,9 +10,12 @@ const Foot = (props) => {
         <div className="Footer-heading">
             <h1>Socials</h1>
             <div className="social-images">
-                <img className="image-socials" src={ FbImage } alt = "Social Fb link"/>
-                <img className="image-socials" src={ InstaImage } alt = "Social Insta Link"/>
-                <img className="image-socials" src={ LinkedInImage } alt = "Social LinkedIn Link"/>
+                <a href="https://www.facebook.com/profile.php?id=100006921404837" target="_blank">
+                <img src={ FbImage } className="image-socials" alt = "Fb social link"/></a>
+                <a href="https://www.instagram.com/rstgt/" target="_blank">
+                <img src={ InstaImage } className="image-socials" alt = "Instagram social link"/></a>
+                <a href="https://www.linkedin.com/in/ripudamanthind/" target="_blank">
+                <img src={ LinkedInImage } className="image-socials" alt = "LinkedIn social link"/></a>
             </div>
         </div>
     </div>
