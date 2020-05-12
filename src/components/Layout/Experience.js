a54b779be2e97bb36331b64b34a8077ed402d61f
Ripudaman <ripudamansinghthind@gmail.com>
2020-05-12 12:34:18 -0700

    changed contents of experience page, resume updated with same content change

--- src/components/Layout/Experience.js
+++ src/components/Layout/Experience.js
@@ -14,9 +14,9 @@ const Experience = (props) => {
           <p>Jan 2020 - April 2020</p>
         </div>
       <div className="experience-column-2">
-      <p>• Developed Mobile Application Prototypes for both iOS and Android using React Native with TypeScript</p>
+      <p>• Developed Mobile Application Prototypes for a Fitness Trainer App providing coaches the ability to manage their clients efficiently for both iOS and Android using React Native, TypeScript & ESlint</p>
       <p>• Led the Development of testing the final prototypes for continuous integration</p>
-      <p>• Contributed in design and dealt with feedback from daily users</p>
+      <p>• Contributed in design and dealt with feedback from daily users </p>
       </div>
       </div>
       <div className="row">
@@ -26,11 +26,10 @@ const Experience = (props) => {
         <p>May 2019 - Dec 2019</p>
           </div>
           <div className="experience-column-2">
-          <p>• Assisted in requirement gathering, design, development and documentation for various enterprise software</p>
-      <p>• Led the design and development of neural networks for species detection of wood improving the results from 56% to 94%</p>
+          <p>• Led the design and development of neural networks for species detection of wood improving the results from 56% to 94%</p>
+      <p>• Assisted in requirement gathering, design, development, and documentation for various AI projects using cntk (Python) to further enhance the defect detection products in the forestry industry</p>
       <p>• Did hands-on work with bioluma sensors to understand and improve how data was being collected and used for various neural networks across the company</p>
-      <p>• Used OpenCV to create proper data samples for the networks</p>
-      <p>• Leveraged knowledge: Git, C++, Python, Django, cntk, OpenCV</p>
+      <p>• Did hands-on work with bioluma laser sensors to understand and improve how data was being collected and used for various neural networks across the company</p>
           </div>
           </div>
           <div className="row">
@@ -40,8 +39,8 @@ const Experience = (props) => {
         <p>Jan 2019 - April 2019</p>
           </div>
           <div className="experience-column-2">
-          <p>• Upgraded the scripts being used by the professor from Perl to Python</p>
-      <p>• Led the coding side of the project. Researched the best approaches that could be incorporated for accurate results low runtime</p>
+          <p>• Upgraded basic biology scripts like DNA to RNA conversion and vice versa being used by the professor from Perl to Python</p>
+          <p>• Led the coding side of the project. Researched the best approaches that could be incorporated for accurate results with low runtimes</p>
           </div>
           </div>
 
