d34ae94fcc13258f6c2dab526e978b97a6446844
Ripudaman <ripudamansinghthind@gmail.com>
2020-05-18 17:38:27 -0700

    added run option for a project

--- src/components/Layout/Projects.js
+++ src/components/Layout/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../css/Projects.css'
 
 import GitHubImage from '../resources/github.png'
+import PlayImage from '../resources/play.png'
 
 export default class FetchGitHub extends React.Component {
   render() {
@@ -13,6 +14,27 @@ export default class FetchGitHub extends React.Component {
                     <p>I have many more projects I constantly work on hosted on my GitHub but these are just the ones that I feel are cool or ongoing</p>
                 </div>
                 <div className="projects-row">
+                <div className="projects-column">
+                            <div className = "projects-column-1">
+                                <h3>Sorting Algorithms Visualization</h3>
+                            </div>
+                            <div className = "projects-column-2">
+                              <p>Work in progress</p>
+                              <br />
+                            <p>Frameworks, Libraries, Languages:</p>
+                            <div className="tech-skills">
+                              <p className="tech-skills-text">ReactJS</p>
+                              <p className="tech-skills-text">Netlify</p>
+                            </div>
+                            <div>
+                              <a href="https://github.com/ripudamansinghthind/sorting-visualization" target="_blank" rel="noopener noreferrer">
+                              <img src={ GitHubImage } className="projects-link-image" alt = "GitHub social link"/></a>
+                              <a href="https://cranky-shirley-2312fb.netlify.app/" target="_blank" rel="noopener noreferrer">
+                              <img src={ PlayImage } className="projects-link-image" alt = "GitHub social link"/></a>
+                            </div>
+                           </div>
+              </div>
+            
                 <div className="projects-column">
                             <div className = "projects-column-1">
                                 <h3>Calculator</h3>
@@ -27,27 +49,10 @@ export default class FetchGitHub extends React.Component {
                               <p className="tech-skills-text">Detox</p>
                             </div>
                             <div>
-                              <a href="https://github.com/ripudamansinghthind" target="_blank" rel="noopener noreferrer">
+                              <a href="https://github.com/ripudamansinghthind/reactnativecalculator" target="_blank" rel="noopener noreferrer">
                               <img src={ GitHubImage } className="projects-link-image" alt = "GitHub social link"/></a>
                             </div>
                             </div>
-                           </div>
-                           <div className="projects-column">
-                            <div className = "projects-column-1">
-                                <h3>Portfolio Website</h3>
-                            </div>
-                            <div className = "projects-column-2">
-                              <p>This portfolio website hosted here.</p>
-                              <br />
-                            <p>Frameworks, Libraries, Languages:</p>
-                            <div className="tech-skills">
-                              <p className="tech-skills-text">React</p>
-                              <p className="tech-skills-text">Netlify</p>
-                            </div>
-                            <div>
-                              <a href="https://github.com/ripudamansinghthind" target="_blank" rel="noopener noreferrer">
-                              <img src={ GitHubImage } className="projects-link-image" alt = "GitHub social link"/></a>
-                            </div></div>
                            </div>
             </div>
             <div className="projects-row">
@@ -63,27 +68,27 @@ export default class FetchGitHub extends React.Component {
                               <p className="tech-skills-text">Python</p>
                             </div>
                             <div>
-                              <a href="https://github.com/ripudamansinghthind" target="_blank" rel="noopener noreferrer">
+                              <a href="https://github.com/ripudamansinghthind/Pathfinding-Algorithm-Visualization" target="_blank" rel="noopener noreferrer">
                               <img src={ GitHubImage } className="projects-link-image" alt = "GitHub social link"/></a>
                             </div>
                             </div>
                            </div>
-                           <div className="projects-column">
+              <div className="projects-column">
                             <div className = "projects-column-1">
-                                <h3>Sorting Algorithms Visualization</h3>
+                                <h3>Portfolio Website</h3>
                             </div>
                             <div className = "projects-column-2">
-                              <p>Work in progress</p>
+                              <p>This portfolio website hosted here.</p>
                               <br />
                             <p>Frameworks, Libraries, Languages:</p>
                             <div className="tech-skills">
-                              <p className="tech-skills-text">Java</p>
+                              <p className="tech-skills-text">ReactJS</p>
+                              <p className="tech-skills-text">Netlify</p>
                             </div>
                             <div>
-                              <a href="https://github.com/ripudamansinghthind" target="_blank" rel="noopener noreferrer">
+                              <a href="https://github.com/ripudamansinghthind/portfolio-website" target="_blank" rel="noopener noreferrer">
                               <img src={ GitHubImage } className="projects-link-image" alt = "GitHub social link"/></a>
-                            </div>
-                           </div>
+                            </div></div>
                            </div>
             </div>
             </div>
